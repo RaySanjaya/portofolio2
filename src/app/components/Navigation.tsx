7f8fb13379d8534afd119b04c0615fac15b7d3b2
Ray Sanjaya <raysanjayagulo@gmail.com>
2024-05-05 23:38:30 +0700

    fix icon navigation

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,3 +1,5 @@
+import { HomeIcon, MessageSquareTextIcon } from "lucide-react";
+
 type NavigationProps = {
   current_menu: number;
   setCurrentMenu: Function;
@@ -5,18 +7,23 @@ type NavigationProps = {
 
 
 export const Navigation = ({ current_menu, setCurrentMenu }: NavigationProps) => {
-  let landingClass: string = 'pe-4 action'
-  let contactMeClass: string = 'pe-4 action'
-  if (current_menu === 1) landingClass = 'pe-4 active'
-  if (current_menu === 3) contactMeClass = 'pe-4 active'
+  let landingClass: string = 'flex flex-row items-center pe-4 action'
+  let contactMeClass: string = 'flex flex-row items-center pe-4 action'
+  if (current_menu === 1) landingClass = 'flex flex-row items-center pe-4 active'
+  if (current_menu === 3) contactMeClass = 'flex flex-row items-center pe-4 active'
 
   return (
     <div className="mb-5" data-aos='zoom-in'>
       <div className="flex justify-center xl:justify-start lg:justify-start md:justify-start sm:justify-start">
         <ul className="flex flex-row">
-          <li className={ landingClass } style={{ cursor: 'pointer' }} onClick={() => { setCurrentMenu(1) }}>Landing</li>
-          {/* <li className="pe-4 action" style={{ cursor: 'pointer' }}>Experience</li> */}
-          <li className={ contactMeClass } style={{ cursor: 'pointer' }} onClick={() => { setCurrentMenu(3) }}>Contact Me</li>
+          <li className={ landingClass } style={{ cursor: 'pointer' }} onClick={() => { setCurrentMenu(1) }}>
+            <HomeIcon size={25} strokeWidth={1.7} absoluteStrokeWidth className="pr-1" />
+            Landing
+          </li>
+          <li className={ contactMeClass } style={{ cursor: 'pointer' }} onClick={() => { setCurrentMenu(3) }}>
+            <MessageSquareTextIcon size={25} strokeWidth={1.7} absoluteStrokeWidth className="pr-1" />
+            Contact Me
+          </li>
         </ul>
       </div>
     </div>
